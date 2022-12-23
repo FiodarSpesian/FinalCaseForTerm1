7a04a0b3b6dd0d285bcf938b6d996f21dcc9b881
Fiodar <fiodarspesian@mail.ru>
2022-12-23 08:10:53 +0300

    add decision of final case

---
+++ CaseDecision/Program.cs
@@ -0,0 +1,44 @@
+﻿
+void FillArray(string[] str, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Enter {i+1} value of array: ");
+        str[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+}
+void PrintArray(string[] str, string massage = "Output given array")
+{
+    Console.WriteLine(massage);
+    int size = str.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(str[i] + " ");
+    }
+    Console.WriteLine("\n");
+}
+void SortArray(string[] str)
+{
+    int size = str.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if(str[i].Length <= 3)
+        {
+            Console.Write(str[i] + " ");
+        }
+    }
+}
+
+void FinalCase()
+{
+    Console.Write("Enter length of the array: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(length);
+    string[] array = new string[length];
+    FillArray(array, length);
+    PrintArray(array);
+    Console.WriteLine("Output new sort array");
+    SortArray(array);
+}
+FinalCase();
